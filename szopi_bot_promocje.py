9fb12a1b4082d6993d3df5ef56cdb5670d7fdf6f
marta-kaminska <kaminskamarta992@gmail.com>
2020-05-16 20:32:46 +0200

    functional change

--- szopi_bot_promocje.py
+++ szopi_bot_promocje.py
@@ -31,10 +31,13 @@ class Promocjebot:
         time.sleep(2)
         password.send_keys(Keys.RETURN)
         time.sleep(5)
-        #wejscie w sklep LIDL
+
+    def sprawdz_promocje(self, nazwa_sklepu):
+    #dostępne nazwy sklepów Lidl, Carrefour, Biedronka
+        bot = self.bot
         sklepy = bot.find_elements_by_class_name("name")
         for element in sklepy:
-            if element.text == "Lidl":
+            if element.text == nazwa_sklepu:
                 element.click()
         time.sleep(3)
         #wejscie w kategorie produktow
@@ -42,24 +45,35 @@ class Promocjebot:
         kategorie.click()
         time.sleep(2)
         #wejscie w aktualne promocje
-        nasze_promocje = bot.find_elements_by_class_name("link")
-        for element in nasze_promocje:
-            if element.text == "Nasze promocje":
-                element.click()
-        time.sleep(3)
-        nazwa_produktu = bot.find_elements_by_class_name("name")
-        stara_cena = bot.find_elements_by_class_name("full-price")
-        nowa_cena = bot.find_elements_by_class_name("price")
-
-        zbior = {}
-        #klucz = nazwa produktu, wartosc klucza stara cena / nowa cena
-        i = 0
-        while i < len(nazwa_produktu):
-            zbior[nazwa_produktu[i].text] = f"{stara_cena[i].text} / {nowa_cena[i].text}"
-            i += 1
-        print(zbior)
+        przyciski = bot.find_elements_by_class_name("link")
+        if len(przyciski) > 2:
+            for element in przyciski:
+                if element.text == "Nasze promocje":
+                    element.click()
+                    time.sleep(3)
+                    nazwa_produktu = bot.find_elements_by_class_name("name")
+                    stara_cena = bot.find_elements_by_class_name("full-price")
+                    nowa_cena = bot.find_elements_by_class_name("price")
+                        
+                    zbior = {}
+                    #klucz = nazwa produktu, wartosc klucza stara cena / nowa cena
+                    i = 0
+                    while i < len(nazwa_produktu):
+                        zbior[nazwa_produktu[i].text] = f"{stara_cena[i].text} / {nowa_cena[i].text}"
+                        i += 1
+                    print(zbior)
+        else:
+            print(f"w sklepie {nazwa_sklepu} aktualnie nie ma promocji")
+    def wylogowanie(self):
+        bot = self.bot
+        bot.find_element_by_class_name("personal-area-menu").click()
+        bot.find_element_by_class_name("logout").click()
+        
+        
     
 
 
-a = Promocjebot("login", "hasło")
+a = Promocjebot("login", "haslo")
 a.login()
+a.sprawdz_promocje("Lidl")
+a.wylogowanie()
